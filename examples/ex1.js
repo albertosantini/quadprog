188bbf2a1051ee7bb3873daddc29d008e319a925
icebox <albertosantini@gmail.com>
2011-09-17 20:00:36 +0200

    Added ex1.R example related to ex1.js one. Fixed path for required module.

--- examples/ex1.js
+++ examples/ex1.js
@@ -4,9 +4,10 @@
 // ## Assume we want to minimize: -(0 5 0) %*% b + 1/2 b^T b
 // ## under the constraints: A^T b >= b0
 // ## with b0 = (-8,2,0)^T
-// ## and (-4 2 0)
+// ## and
+// ##     (-4 2  0)
 // ## A = (-3 1 -2)
-// ## ( 0 0 1)
+// ##     ( 0 0  1)
 // ## we can use solve.QP as follows:
 // ##
 // Dmat <- matrix(0,3,3)
@@ -16,7 +17,7 @@
 // bvec <- c(-8,2,0)
 // solve.QP(Dmat,dvec,Amat,bvec=bvec)
 
-var qp = require('quadprog');
+var qp = require('../lib/quadprog');
 
 var Dmat = [], dvec = [], Amat = [], bvec = [], res;
 
