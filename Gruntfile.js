b7cfdb6ee313424dbb15129e02d67e1095c5c053
icebox <albertosantini@gmail.com>
2016-01-28 17:31:43 +0100

    Update eslint 2.0.0-beta.2.
    Remove tasks, lint and vows, from Gruntfile.js
    The idea is removing grunt, but benchmark is a longer task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,6 @@
 
 module.exports = function (grunt) {
     grunt.initConfig({
-        eslint: {
-            src: [
-                "Gruntfile.js",
-                "index.js",
-                "lib/**/*.js",
-                "test/**/*.js"
-            ]
-        },
-
-        vows: {
-            all: {
-                src: "test/*-test.js",
-                options: {
-                    reporter: "spec",
-                    error: false
-                }
-            }
-        },
-
         benchmark: {
             all: {
                 src: [
@@ -34,13 +15,9 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks("grunt-eslint");
-    grunt.loadNpmTasks("grunt-vows-runner");
     grunt.loadNpmTasks("grunt-benchmark");
 
     grunt.registerTask("default", [
-        "eslint",
-        "vows",
         "benchmark"
     ]);
 };
