d6dcffa76cc0f87887337f1b3123ea7f4b75813f
icebox <albertosantini@gmail.com>
2019-03-17 11:39:08 +0100

    Reformat tests source code.
    Done with the help of prettier.

--- test/solution-test.js
+++ test/solution-test.js
@@ -22,22 +22,19 @@ function almostEqualArray(a, b) {
 }
 
 function testWrapper(base) {
-    const {
-        Dmat,
-        dvec,
-        Amat,
-        bvec,
-        meq,
-        factorized
-    } = JSON.parse(fs.readFileSync(`test/${base}-data.json`).toString());
-    const expected = JSON.parse(fs.readFileSync(`test/${base}-result.json`).toString());
+    const { Dmat, dvec, Amat, bvec, meq, factorized } = JSON.parse(
+        fs.readFileSync(`test/${base}-data.json`).toString()
+    );
+    const expected = JSON.parse(
+        fs.readFileSync(`test/${base}-result.json`).toString()
+    );
 
     [Dmat, Amat].forEach(m => m.forEach(r => r.unshift(0)));
     [Dmat, dvec, Amat, bvec].forEach(v => v.unshift(0));
-    if (expected.Lagrangian.length === undefined) { // eslint-disable-line no-undefined
+    if (!expected.Lagrangian.length) {
         expected.Lagrangian = [expected.Lagrangian];
     }
-    if (expected.iact.length === undefined) { // eslint-disable-line no-undefined
+    if (!expected.iact.length) {
         expected.iact = [expected.iact];
     }
 
@@ -52,24 +49,42 @@ function testWrapper(base) {
             iterations
         } = qp.solveQP(Dmat, dvec, Amat, bvec, meq, [, factorized ? 1 : 0]); // eslint-disable-line no-sparse-arrays
 
-        [solution, unconstrained_solution, Lagrangian, iact, iterations].forEach(v => v.shift()); // eslint-disable-line camelcase
+        [
+            solution,
+            unconstrained_solution, // eslint-disable-line camelcase
+            Lagrangian,
+            iact,
+            iterations
+        ].forEach(v => v.shift());
 
-        t.equal(message, "",
-            "message is empty");
-        t.ok(almostEqual(value[1], expected.value),
-            "values are almost equal");
-        t.ok(almostEqualArray(solution, expected.solution),
-            "solutions are almost equal");
-        t.ok(almostEqualArray(unconstrained_solution, expected["unconstrained.solution"]),
-            "unconstrained solutions are almost equal");
-        t.ok(almostEqualArray(Lagrangian, expected.Lagrangian),
-            "lagrangians are almost equal");
-        t.deepEqual(iact.slice(0, expected.iact.length), expected.iact,
-            "iact values are equal");
-        t.deepEqual(iact.slice(expected.iact.length), new Array(iact.length - expected.iact.length).fill(0),
-            "the rest of iact values are equal");
-        t.deepEqual(iterations, expected.iterations,
-            "iterations are equal");
+        t.equal(message, "", "message is empty");
+        t.ok(almostEqual(value[1], expected.value), "values are almost equal");
+        t.ok(
+            almostEqualArray(solution, expected.solution),
+            "solutions are almost equal"
+        );
+        t.ok(
+            almostEqualArray(
+                unconstrained_solution,
+                expected["unconstrained.solution"]
+            ),
+            "unconstrained solutions are almost equal"
+        );
+        t.ok(
+            almostEqualArray(Lagrangian, expected.Lagrangian),
+            "lagrangians are almost equal"
+        );
+        t.deepEqual(
+            iact.slice(0, expected.iact.length),
+            expected.iact,
+            "iact values are equal"
+        );
+        t.deepEqual(
+            iact.slice(expected.iact.length),
+            new Array(iact.length - expected.iact.length).fill(0),
+            "the rest of iact values are equal"
+        );
+        t.deepEqual(iterations, expected.iterations, "iterations are equal");
         t.end();
     }
     return wrappedTest;
